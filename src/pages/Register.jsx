865c5e8e45751ebeb1818ecb5e49e77ff3206c22
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-04-27 15:27:03 +0300

    Ensure imports are made from the correct src folder for both Authformand authcontext.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useAuth } from './context/AuthContext'
-import AuthForm from './components/AuthForm'
+import { useAuth } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
 
 const Register = () => {
   const { register } = useAuth()
